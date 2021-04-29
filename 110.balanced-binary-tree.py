99a87637cdf514af25efe4413b57830a0ffa2921
Kartikey <ranjankartikey1999@gmail.com>
2021-04-30 01:31:11 +0530

    updated files

--- 110.balanced-binary-tree.py
+++ 110.balanced-binary-tree.py
@@ -13,6 +13,14 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        
-# @lc code=end
+        def check(root):
+            if root is None:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+        return check(root) != -1
 
+# @lc code=end
