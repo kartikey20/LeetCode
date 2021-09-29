52c5fa51e122a4d0a0ba1a6cb1f266a6bb8e522a
Kartikey <ranjankartikey1999@gmail.com>
2021-09-29 17:06:08 +0530

    Happy Number

---
+++ 202. Happy Number.py
@@ -0,0 +1,15 @@
+def isHappy(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    s = set()
+    while n != 1:
+        n = sum([int(i) ** 2 for i in str(n)])
+        if n in s:
+            return False
+        s.add(n)
+    return True
+
+
+print(isHappy(19))
